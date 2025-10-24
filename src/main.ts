0864fec60e76e604b6804d602ff74522cf49f3c3
GabrielJAllen <157070900+GabrielJAllen@users.noreply.github.com>
2025-10-24 22:38:41 +0000

    changed listeners to push to a 2d array of points as well as redo and undo buttons (Step 4 and 5 Complete)

--- src/main.ts
+++ src/main.ts
@@ -4,53 +4,88 @@ document.body.innerHTML = `
   <h1>Current Header</h1>
   <canvas id="canvas" class="canvas"></canvas>
   <p><button id="clear">Clear</button></p>
+  <button id="undo">Undo</button>
+  <button id="redo">Redo</button>
 `;
 
 const canvas = <HTMLCanvasElement> document.getElementById("canvas");
 const pad = <CanvasRenderingContext2D> canvas.getContext("2d");
 const clear = <HTMLButtonElement> document.getElementById("clear");
-const cursor = { x: 0, y: 0, drawing: false };
+const undo = <HTMLButtonElement> document.getElementById("undo");
+const redo = <HTMLButtonElement> document.getElementById("redo");
 
-//drawing functions below based off code from https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+const cursor = { x: 0, y: 0, drawing: false };
+const lines: [[{ x: number; y: number }]] = [
+  <[{ x: number; y: number }]> <unknown> [],
+];
+const redoLines: [[{ x: number; y: number }]] = [
+  <[{ x: number; y: number }]> <unknown> [],
+];
+
+let currentLine: [{ x: number; y: number }];
+
 canvas.addEventListener("mousedown", (e) => {
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
   cursor.drawing = true;
+
+  currentLine = <[{ x: number; y: number }]> <unknown> [];
+  lines.push(currentLine);
+  redoLines.splice(0, redoLines.length);
+  currentLine.push({ x: cursor.x, y: cursor.y });
+
+  canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.drawing) {
-    drawLine(pad, cursor.x, cursor.y, e.offsetX, e.offsetY);
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    currentLine.push({ x: cursor.x, y: cursor.y });
+
+    canvas.dispatchEvent(new Event("drawing-changed"));
   }
 });
 
-canvas.addEventListener("mouseup", (e) => {
+canvas.addEventListener("mouseup", () => {
   if (cursor.drawing) {
-    drawLine(pad, cursor.x, cursor.y, e.offsetX, e.offsetY);
-    cursor.x = 0;
-    cursor.y = 0;
     cursor.drawing = false;
+    currentLine = <[{ x: number; y: number }]> <unknown> [];
+
+    canvas.dispatchEvent(new Event("drawing-changed"));
   }
 });
 
-function drawLine(
-  context: CanvasRenderingContext2D,
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number,
-) {
-  context.beginPath();
-  context.strokeStyle = "black";
-  context.lineWidth = 1;
-  context.moveTo(x1, y1);
-  context.lineTo(x2, y2);
-  context.stroke();
-  context.closePath();
-} //end of code based off of https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+canvas.addEventListener("drawing-changed", () => {
+  pad.clearRect(0, 0, canvas.width, canvas.height);
+  for (const line of lines) {
+    if (line.length > 0) {
+      pad.beginPath();
+      const { x, y } = line[0];
+      pad.moveTo(x, y);
+      for (const { x, y } of line) {
+        pad.lineTo(x, y);
+      }
+      pad.stroke();
+    }
+  }
+});
 
 clear.addEventListener("click", () => {
-  pad.clearRect(0, 0, canvas.width, canvas.height);
+  lines.splice(0, lines.length);
+  canvas.dispatchEvent(new Event("drawing-changed"));
+});
+
+undo.addEventListener("click", () => {
+  if (lines.length > 0) {
+    redoLines.push(<[{ x: number; y: number }]> lines.pop());
+    canvas.dispatchEvent(new Event("drawing-changed"));
+  }
+});
+
+redo.addEventListener("click", () => {
+  if (redoLines.length > 0) {
+    lines.push(<[{ x: number; y: number }]> redoLines.pop());
+    canvas.dispatchEvent(new Event("drawing-changed"));
+  }
 });
